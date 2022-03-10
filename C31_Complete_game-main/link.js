79a4c322d74965066d924f5af6c8fddb3e1f2c4f
priyanka-sidhuraj <71912936+priyanka-sidhuraj@users.noreply.github.com>
2022-03-10 13:30:08 +0530

    Add files via upload

---
+++ C31_Complete_game-main/link.js
@@ -0,0 +1,20 @@
+class Link{
+    constructor(bodyA,bodyB)
+    {
+     this.link = Constraint.create(
+        {
+          bodyA:bodyA.body.bodies[6],
+          bodyB:bodyB,
+          length:-10,
+          stiffness:0.01
+        });
+        World.add(engine.world,this.link);
+    } 
+
+    detach()
+    {
+      World.remove(engine.world,this.link);
+     
+    }
+}
+
